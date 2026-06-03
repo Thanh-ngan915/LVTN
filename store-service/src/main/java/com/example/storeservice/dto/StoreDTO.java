328c18f600f09b7fa7127a8ae1ab0d8d35897f84
Trần Nguyễn Thu Ngân <thungan050804@gmail.com>
2026-06-03 16:45:45 +0700

    manage shops

--- store-service/src/main/java/com/example/storeservice/dto/StoreDTO.java
+++ store-service/src/main/java/com/example/storeservice/dto/StoreDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,4 +18,8 @@ public class StoreDTO {
     private String location;
     private String description;
     private String status;
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
 }
