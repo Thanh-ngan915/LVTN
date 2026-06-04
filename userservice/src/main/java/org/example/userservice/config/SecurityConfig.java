20b7de64b15cb21a8ee6d82617a6ad0d6174e26d
Trần Nguyễn Thu Ngân <thungan050804@gmail.com>
2026-06-04 23:14:46 +0700

    fix register become seller

--- userservice/src/main/java/org/example/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/org/example/userservice/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/users/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/users/admin/**").hasAnyRole("ADMIN", "SERVICE")
                         .requestMatchers("/api/users/**").authenticated()
                         .anyRequest().authenticated()
                 );
